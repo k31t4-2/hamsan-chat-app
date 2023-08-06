70a61dc487c804460ff8bbffc139b3fee09887f0
KEITA <ksekimotonz@gmail.com>
2023-08-06 23:39:21 +0900

    色々整理できた。

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
-import React from 'react'
+import { useState } from 'react'
 import './App.css'
 
+import SignIn from './components/SignIn'
+import Main from './components/Main';
+import  {signInEnabled}  from "../config.json";
+
 function App() {
+
+  const [name, setName] = useState("")
+  console.log(name);
+
   return (
-    <>
-    </>
+    (signInEnabled && name === "") ? (
+      <SignIn setName={setName}/>
+  ): (
+      <Main name={name}/>
+    )
   )
 }
 
---
+++ src/components/Main.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Main = (props) => {
+
+  const { name } = props;
+
+  return (
+    <div>Main</div>
+  )
+}
+
+export default Main
